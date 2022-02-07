b84669ff4b72fe3e1cf54311c4a72f269bafff37
Сергей <95522384+Sergio666666@users.noreply.github.com>
2022-02-07 15:05:28 +0300

    Update tickets.py

--- tickets.py
+++ tickets.py
@@ -4,16 +4,16 @@ for i in range(1, ticket + 1):
     age = int(input(f"Age {1} participant's "))
     if age < 18:
         price.append(0)
-     elif 18 < age < 25:   
-         price.append(990)
-     elif age > 25:   
-         price.append(1390)
- print(price)
- if ticket > 3:
-       a = int(sum(price) - sum(price)/10)
-       print("your purchase amount is:" , a)
- else:
-        a = sum(price)
-        print ("your purchase amount is: ", a)
- print(price)  
+    elif 18 < age < 25:   
+        price.append(990)
+    elif age > 25:   
+        price.append(1390)
+print(price)
+if ticket > 3:
+      a = int(sum(price) - sum(price)/10)
+      print("your purchase amount is:" , a)
+else:
+       a = sum(price)
+       print ("your purchase amount is: ", a)
+print(price)  
  
