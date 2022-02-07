c07624480e7856b214362e5c34ceeadbfe772b7e
Сергей <95522384+Sergio666666@users.noreply.github.com>
2022-02-07 15:11:00 +0300

    Update tickets.py

--- tickets.py
+++ tickets.py
@@ -1,7 +1,7 @@
 ticket = int(input('How many tickets would you like to buy?'))
 price = []
 for i in range(1, ticket + 1):
-    age = int(input(f"Age {1} participant's "))
+    age = int(input(f"Age {i} participant's "))
     if age < 18:
         price.append(0)
     elif 18 < age < 25:   
